3bf49828f77cc6f16e09f5f8f9313ed2a684d0a1
HighlySupreme <gal.1999k@gmail.com>
2021-07-19 02:27:22 +0200

    added js for the sidebar and for sections

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*scroll on click effect*/
+$(function(){
+    $(".colorPop li a").on('click', function(){
+        $("html, body").animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
+
+/*mobile navbar close on click*/
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
